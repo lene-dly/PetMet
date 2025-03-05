077390fbff59d049a7aed0b29fa1aedbcd1ff78c
Bernalene Dulay <dulaybernalene96@gmail.com>
2025-03-06 01:13:09 +0800

    add settings

--- PetMet/settings.py
+++ PetMet/settings.py
@@ -21,7 +21,7 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-ALLOWED_HOSTS = ['10.0.2.2', 'localhost', '127.0.0.1', '.onrender.com']
+ALLOWED_HOSTS = ['10.0.2.2', 'localhost', '127.0.0.1', '.onrender.com', 'https://petmet-n5ae.onrender.com']
 
 
 
